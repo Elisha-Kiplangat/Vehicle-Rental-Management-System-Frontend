7c6be2665e120a7b956a4c676b3e933276b7a3c3
Elisha-Kiplangat <elishakiplanga@gmail.com>
2024-07-07 15:57:26 +0300

    update

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,27 +1,44 @@
+import { useState } from 'react';
+import SignInUp from '../pages/SignInUp'; 
 
 const Nav = () => {
+  const [showSignInUp, setShowSignInUp] = useState(false);
+
+  const handleAccountClick = () => {
+    setShowSignInUp(true); 
+  };
+
+  const handleCloseSignInUp = () => {
+    setShowSignInUp(false); 
+  };
+
   return (
     <>
-      <div className="navbar bg-blue-100">
-  <div className="navbar-start ml-10">
-      <div tabIndex={0} role="button" className="btn btn-ghost">
-        Car Rental
+      <div className="navbar bg-blue-100 relative z-10">
+        <div className="navbar-start ml-10">
+          <div tabIndex={0} role="button" className="btn btn-ghost">
+            Car Rental
+          </div>
+        </div>
+        <div className="navbar-center lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            <li><a>Home</a></li>
+            <li><a>About Us</a></li>
+            <li><a>Contact Us</a></li>
+          </ul>
+        </div>
+        <div className="navbar-end mr-10">
+          <a className="btn" onClick={handleAccountClick}>Account</a>
+        </div>
       </div>
-  </div>
-  <div className="navbar-center lg:flex">
-    <ul className="menu menu-horizontal px-1">
-      <li><a>Home</a></li>
-      <li><a>About Us</a></li>
-      <li><a>Contact Us</a></li>
-    </ul>
-  </div>
-  <div className="navbar-end mr-10">
-    <a className="btn">Account</a>
-  </div>
-</div>
-    
-    </>
-  )
-}
 
-export default Nav
+      {showSignInUp && (
+        <div className="fixed top-0 left-0 w-full h-full bg-gray-800 bg-opacity-50 flex justify-center items-center z-50">
+          <SignInUp onClose={handleCloseSignInUp} /> {/* Render SignInUp component with onClose prop */}
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Nav;
