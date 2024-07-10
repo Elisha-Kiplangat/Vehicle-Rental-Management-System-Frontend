cf2544ea342366ae16c10b7f5c8e5785b404b5ab
Elisha-Kiplangat <elishakiplanga@gmail.com>
2024-07-10 21:13:43 +0300

    userslice

---
+++ src/features/user/UserSlice.ts
@@ -0,0 +1,30 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { LoginResponse } from '../auth/AuthSlice';
+
+export interface UserState {
+    id: number | null;
+    token: string | null;
+}
+
+const initialState: UserState = {
+    id: null,
+    token: null,
+};
+
+const userSlice = createSlice({
+    name: 'user',
+    initialState,
+    reducers: {
+        setUser: (state, action: PayloadAction<LoginResponse>) => {
+            state.id = action.payload.id;
+            state.token = action.payload.token;
+        },
+        clearUser: (state) => {
+            state.id = null;
+            state.token = null;
+        },
+    },
+});
+
+export const { setUser, clearUser } = userSlice.actions;
+export default userSlice.reducer;
