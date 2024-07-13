542c44003cc5ae7941331a5665e43406eb108a75
Elisha-Kiplangat <elishakiplanga@gmail.com>
2024-07-13 19:41:25 +0300

    added slice imports

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,14 +2,20 @@ import { configureStore } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/query/react';
 import { apiSlice } from '../features/auth/AuthSlice';
 import userReducer from '../features/user/UserSlice';
+import { bookingApi } from '../features/BookingAPI';
+import { vehiclesApi } from '../features/VehiclesAPI';
+import { messagesApi } from '../features/MessagesApi';
 
 export const store = configureStore({
     reducer: {
         [apiSlice.reducerPath]: apiSlice.reducer,
+        [bookingApi.reducerPath]: bookingApi.reducer,
+        [vehiclesApi.reducerPath]: vehiclesApi.reducer,
+        [messagesApi.reducerPath]: messagesApi.reducer,
         user: userReducer,
     },
     middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware().concat(apiSlice.middleware),
+        getDefaultMiddleware().concat(apiSlice.middleware, bookingApi.middleware, vehiclesApi.middleware, messagesApi.middleware),
 });
 
 setupListeners(store.dispatch);
